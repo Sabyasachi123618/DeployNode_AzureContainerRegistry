eafcdc8eb4cde54666ac6ea3576475f55a8db028
Sabyasachi Banerjee <57725708+Sabyasachi123618@users.noreply.github.com>
2024-03-31 14:53:34 +0530

    Initial Code Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const Memer = require("random-jokes-api");
+// Create an Express application
+const app = express();
+
+// Define routes
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.get("/getJoke",(req,res)=>{
+    let jokes = Memer.joke();
+    res.send(jokes);
+})
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
